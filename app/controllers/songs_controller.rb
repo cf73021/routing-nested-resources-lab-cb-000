c61a18aa8da6344c18cc1cc0657641647695fbe6
Chris Franklin <cf73021@gmail.com>
2018-12-19 19:58:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,9 @@
 class SongsController < ApplicationController
   def index
-    if params[:artist_name]
-      @songs = Artist.find(params[:artist_name]).songs
+    if params[:artist_id]
+      @songs = Artist.find(params[:artist_id]).songs
     else
-      @songs = Song.all
+      redirect_to @song
     end
   end
 
